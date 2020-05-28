d541dbb880c6253c7f48ead849b8083c26a5a563
Philip Damra <pdamra@dealerinspire.com>
2020-05-27 22:56:06 -0700

    Debug

--- services/users/src/services/users/users.class.ts
+++ services/users/src/services/users/users.class.ts
@@ -1,11 +1,11 @@
-import { Service, KnexServiceOptions } from 'feathers-knex';
+import { KnexServiceOptions, Service } from 'feathers-knex';
 import { Application } from '../../declarations';
 
 export class Users extends Service {
   constructor(options: Partial<KnexServiceOptions>, app: Application) {
     super({
       ...options,
-      name: 'users'
+      name: 'users',
     });
   }
 }
