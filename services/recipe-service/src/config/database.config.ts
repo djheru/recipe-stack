e7695156094b606b451812bae0d359d189a27ccf
Philip Damra <pdamra@dealerinspire.com>
2020-06-05 15:25:59 -0700

    Configuration pt 2

--- services/recipe-service/src/config/database.config.ts
+++ services/recipe-service/src/config/database.config.ts
@@ -1,11 +1,14 @@
 import { registerAs } from '@nestjs/config';
 
+console.log({ env: process.env.DB_SYNC });
 const {
+  DB_ENGINE: type = 'postgres',
   DB_HOST: host = 'localhost',
   PORT: port = 5432,
   DB_USERNAME: username = 'postgres',
   DB_PASSWORD: password = 'postgres',
   DB_NAME: database = 'recipes',
+  DB_SYNC: synchronize = false,
 } = process.env;
 
 export default registerAs('database', () => ({
@@ -14,4 +17,6 @@ export default registerAs('database', () => ({
   username,
   password,
   database,
+  type,
+  synchronize: synchronize && synchronize !== 'false',
 }));
--- services/recipe-service/src/main.ts
+++ services/recipe-service/src/main.ts
@@ -1,8 +1,15 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
 
+dotenv.config();
+const { ADDRESS = '0.0.0.0', NAME, PORT = 9000 } = process.env;
+
 async function bootstrap() {
+  const logger = new Logger('AppBootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT, ADDRESS);
+  logger.log(`${NAME} application running on: ${ADDRESS}:${PORT}`);
 }
 bootstrap();
