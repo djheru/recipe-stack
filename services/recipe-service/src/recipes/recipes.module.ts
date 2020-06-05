d0371eb30dffcc1092fb2730c307cbe4d6fe6157
Philip Damra <pdamra@dealerinspire.com>
2020-06-05 15:29:42 -0700

    Generate new controller

--- services/recipe-service/src/recipes/recipes.module.ts
+++ services/recipe-service/src/recipes/recipes.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { RecipesController } from './recipes.controller';
 
-@Module({})
+@Module({
+  controllers: [RecipesController]
+})
 export class RecipesModule {}
