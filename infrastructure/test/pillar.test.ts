208ceab9d15bc09e62d777b1d4dad1609ca1d5fc
Philip Damra <pdamra@dealerinspire.com>
2020-06-28 14:11:04 -0700

    Fix tests

--- infrastructure/test/pillar.test.ts
+++ infrastructure/test/pillar.test.ts
@@ -1,5 +1,4 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import { Repository } from '@aws-cdk/aws-codecommit';
+import { expect as expectCDK, MatchStyle, matchTemplate } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as Pillar from '../lib/pillar-stack';
 
@@ -7,7 +6,7 @@ test('Empty Stack', () => {
   const app = new cdk.App();
 
   // WHEN
-  const stack = new Pillar.PillarStack(app, 'MyTestStack', {});
+  const stack = new Pillar.PillarStack(app, 'MyTestStack', { environmentName: 'dev', hostedZoneDomainName: 'example.com' });
   // THEN
   expectCDK(stack).to(
     matchTemplate(
