94983748722dd8bf9c4e8ff3ca306ef94875856c
Philip Damra <pdamra@dealerinspire.com>
2020-07-12 22:34:45 -0700

    receive access token and verify if the request is authorized or not

---
+++ services/recipe-service/src/auth/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { passportJwtSecret } from 'jwks-rsa';
+
+const {
+  AUTH0_DOMAIN: auth0Domain
+} = process.env;
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      secretOrKeyProvider: passportJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `${auth0Domain}.well-known/jwks.json`
+      })
+    });
+  }
+
+  validate(payload: any) {
+    return payload;
+  }
+}
